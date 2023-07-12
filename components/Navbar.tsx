a482cae3fd922928312c2083733e36b94a25b803
evyatarhaim10455 <evyatar@conseeierge.com>
2023-07-13 01:58:41 +0300

    creating the first page

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
+const Navbar = () => {
+  return (
+    <header w-full absolute z-10>
+      <nav className="max-w-[1440px] mx-auto"></nav>
+    </header>
+  );
+};
+
+export default Navbar;
